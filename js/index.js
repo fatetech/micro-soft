95d1ac0ff4f4971d8931fc26ef4fd840e377490e
iamFate <stephalex222@gmail.com>
2023-04-27 01:37:59 -0700

    update

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// document.querySelector('.menu-btn').addEventListener
+//        ('click', () => document.querySelector('.left')
+//        .classList.toggle('show'));
+
+const moveUp = document.querySelector('#top');
+
+moveUp.onclick= function(){
+    window.scrollTo(0,0)
+}
